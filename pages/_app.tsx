688de39f42bba95262481173607aa586470eac45
Frederick Bejarano Sanchez <bejarano.frederick@gmail.com>
2021-11-17 10:06:17 -0500

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,48 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloLink } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
+
+// const dataNormalizers = new ApolloLink((operation, forward) => {
+//   return forward(operation).map((response) => {
+//     if (response?.data?.lotCollection) {
+//       const lots = response?.data?.lotCollection?.items;
+//       response.data = {
+//         lotCollection: {
+//           items: lots.filter((e: ILotData) => !!e.sys?.publishedAt),
+//         },
+//       };
+//     }
+
+//     return response;
+//   });
+// });
+
+const authLink = setContext(async (_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer 19vUSnF3_8S-OsepxXBcDAI_Ua3GbwSy5c7HNTXB-R0`,
+    },
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: 'https://graphql.contentful.com/content/v1/spaces/fu9did2d8yaw/environments/staging',
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
