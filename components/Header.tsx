2071cc91ac35936a9901878fbfff64a1d49cb3f0
jpirazusta <52186485+jpirazusta@users.noreply.github.com>
2021-11-18 12:49:31 -0300

    Authentication (#3)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,45 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0";
 import styles from "styles/Header.module.css";
 
-const Header = () => (
-  <div className={styles.container}>
-    <Link href="/">
-      <a><Image src="/images/metaverso-logo.svg" alt="logo" width={237} height={40} /></a>
-    </Link>
-    <button className={styles.button}>
-      SIGN UP
-      <div className={styles.icon}>
-        <Image src="/icons/twitter.svg" alt="icon" width={19} height={15} />
-      </div>
-    </button>
-  </div>
-)
+const Header = () => {
+  const { user } = useUser();
+
+  return (
+    <div className={styles.container}>
+      <Link href="/">
+        <a>
+          <Image
+            src="/images/metaverso-logo.svg"
+            alt="logo"
+            width={237}
+            height={40}
+          />
+        </a>
+      </Link>
+      {user ? (
+        <a href="/api/auth/logout">
+          <button className={styles.button}>logout</button>
+        </a>
+      ) : (
+        <a href="/api/auth/login">
+          <button className={styles.button}>
+            SIGN UP
+            <div className={styles.icon}>
+              <Image
+                src="/icons/twitter.svg"
+                alt="icon"
+                width={19}
+                height={15}
+              />
+            </div>
+          </button>
+        </a>
+      )}
+    </div>
+  );
+};
 
 export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 
 import type { AppProps } from 'next/app';
+import { UserProvider } from '@auth0/nextjs-auth0';
 import Header from 'components/Header';
 import 'styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <UserProvider>
       <Header />
       <Component {...pageProps} />
-    </>
+    </UserProvider>
   )
 }
 
