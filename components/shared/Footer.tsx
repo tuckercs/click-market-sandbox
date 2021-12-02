04f685a23550fb2f1c55356ffb7e40e6e440ce57
Frederick Bejarano Sanchez <bejarano.frederick@gmail.com>
2021-12-02 12:17:09 -0500

    fix footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "styles/Footer.module.css";
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <a href="/" className={styles.footerText}>
-        Health and Safety Guidelines
-      </a>
+      <Link href="/">
+        <a className={styles.footerText}>Health and Safety Guidelines</a>
+      </Link>
       <div className={styles.footerLogo}>
         <Image
           src="/images/metaverso-logo.svg"
